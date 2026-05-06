0b670e40b04ef6059362ab28ebc7cb5af300885c
MajBregar <maj.bregar@gmail.com>
2026-05-06 17:29:08 +0200

    Cleaner Working Enviroment

---
+++ RenderingPipeline.js
@@ -0,0 +1,135 @@
+import {Camera, Node, Transform} from 'engine/core.js';
+import { SplatRenderer } from './SplatRenderer.js';
+import { Compositor } from './Compositor.js';
+
+export class RenderingPipeline {
+
+    constructor({
+        device,
+        context,
+        format,
+        canvas,
+        scene,
+        camera,
+    }) {
+
+        this.device = device;
+        this.context = context;
+        this.format = format;
+
+        this.canvas = canvas;
+        this.scene = scene;
+        this.camera = camera;
+
+        this.renderer = new SplatRenderer(device, format);
+
+        this.compositorFloat = new Compositor(device, 'rgba32float');
+
+        this.compositor = new Compositor(device, format);
+        this.compositor.gamma = 1;
+
+        //BUFFERS / TEXTURES
+        this.depthTexture = null;
+        this.colorTexture = null;
+        this.compositorTexture = null;
+
+        // STATE VARIABLES
+        this.nFrames = 0;
+    }
+
+    // CALLABLE
+
+    instantResetHandler() {
+        this.#resetAccumulation()
+    }
+
+    update(t, dt) {
+        // nothing
+    }
+
+    resize(width, height) {
+        this.camera.getComponentOfType(Camera).aspect = width / height;
+        this.instantResetHandler();
+    }
+
+    render() {
+        this.#resizeColorTexture();
+        this.#resizeDepthTexture();
+        this.#resizeCompositorTexture();
+
+        const renderTarget = {
+            color: this.colorTexture,
+            depth: this.depthTexture,
+        };
+
+        const compositorTarget = {
+            color: this.compositorTexture,
+        };
+
+        const canvasTarget = {
+            color: this.context.getCurrentTexture(),
+        };
+
+        this.renderer.render(renderTarget, this.scene, this.camera);
+
+        this.nFrames += 1;
+        this.compositorFloat.render(compositorTarget, this.colorTexture, 1 / this.nFrames, 1);
+
+        this.compositor.render(canvasTarget,this.compositorTexture);
+    }
+
+
+    // INTERNAL
+    
+    #resetAccumulation() {
+        this.nFrames = 0;
+    }
+
+    #resizeColorTexture() {
+        if (this.colorTexture && this.colorTexture.width === this.canvas.width &&  this.colorTexture.height === this.canvas.height) {
+            return;
+        }
+
+        this.colorTexture?.destroy();
+        this.colorTexture = this.device.createTexture({
+            size: [
+                this.canvas.width,
+                this.canvas.height,
+            ],
+            format: this.format,
+            usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
+        });
+    }
+
+    #resizeDepthTexture() {
+        if (this.depthTexture && this.depthTexture.width === this.canvas.width && this.depthTexture.height === this.canvas.height) {
+            return;
+        }
+
+        this.depthTexture?.destroy();
+        this.depthTexture = this.device.createTexture({
+            size: [
+                this.canvas.width,
+                this.canvas.height,
+            ],
+            format: 'depth24plus',
+            usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        });
+    }
+
+    #resizeCompositorTexture() {
+        if (this.compositorTexture && this.compositorTexture.width === this.canvas.width && this.compositorTexture.height === this.canvas.height) {
+            return;
+        }
+
+        this.compositorTexture?.destroy();
+        this.compositorTexture = this.device.createTexture({
+            size: [
+                this.canvas.width,
+                this.canvas.height,
+            ],
+            format: 'rgba32float',
+            usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
+        });
+    }
+}
---
+++ SplatLoader.js
@@ -0,0 +1,106 @@
+import { vec3 } from 'glm';
+
+import { Node } from 'engine/core.js';
+
+import { parseSplats } from './parseSplats.js';
+import { Splat } from './Splat.js';
+
+export class SplatLoader {
+
+    constructor({
+        canvas,
+        splatContainer,
+        renderingPipeline,
+        defaultFile = null,
+    }) {
+
+        this.canvas = canvas;
+        this.splatContainer = splatContainer;
+        this.renderingPipeline = renderingPipeline;
+
+        this.defaultFile = defaultFile;
+    }
+
+    async initialize() {
+        if (this.defaultFile) {
+            const loaded = await this.#tryLoadDefaultFile();
+            if (loaded) {
+                return;
+            }
+        }
+        //console.log('Waiting for drag and drop...');
+    }
+
+    enableDragAndDrop() {
+        this.canvas.addEventListener('dragover', e => {
+            e.preventDefault();
+        });
+
+        this.canvas.addEventListener('drop', async e => {
+            e.preventDefault();
+            const arrayBuffers = await Promise.all(
+                [...e.dataTransfer.files].map(
+                    file => file.arrayBuffer()
+                )
+            );
+            await this.loadFromArrayBuffers(arrayBuffers);
+        });
+    }
+
+    async loadFromArrayBuffers(arrayBuffers) {
+        this.#clearSplats();
+
+        for (const arrayBuffer of arrayBuffers) {
+            const splatData = parseSplats(arrayBuffer);
+
+            this.#centerSplats(splatData);
+
+            const splat = new Node();
+            splat.addComponent(new Splat(splatData));
+
+            this.splatContainer.addChild(splat);
+        }
+
+        this.renderingPipeline.instantResetHandler();
+    }
+
+    async #tryLoadDefaultFile() {
+        try {
+            //console.log(`Attempting to load ${this.defaultFile}`);
+
+            const response = await fetch(this.defaultFile);
+
+            if (!response.ok) {
+                throw new Error('File not found');
+            }
+
+            const arrayBuffer = await response.arrayBuffer();
+
+            await this.loadFromArrayBuffers([arrayBuffer]);
+            //console.log('Loaded local splat file');
+
+            return true;
+        } catch (err) {
+            //console.log('No local splat file found');
+            return false;
+        }
+    }
+
+    #clearSplats() {
+        for (const child of this.splatContainer.children) {
+            child.remove();
+        }
+    }
+
+    #centerSplats(splatData) {
+        const splatMean = splatData
+            .map(splat => splat.position)
+            .reduce(
+                (a, p) => vec3.add(a, a, vec3.scale(vec3.create(), p, 1 / splatData.length)), vec3.create()
+            );
+
+        for (const splat of splatData) {
+            vec3.subtract(splat.position, splat.position, splatMean);
+        }
+    }
+}
